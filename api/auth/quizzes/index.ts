40ed7e70f4c6f9a909b31379f7ab50a24d318dd5
romesh-jaya <monty.rom.jay@gmail.com>
2022-11-08 15:53:27 +0530

    feat: refactor code

--- api/auth/quizzes/index.ts
+++ api/auth/quizzes/index.ts
@@ -10,16 +10,7 @@ export interface ISignupError {
   isGeneralError?: boolean;
 }
 
-export default async function (req: VercelRequest, res: VercelResponse) {
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', 'POST');
-    return res.status(405).end('Method Not Allowed');
-  }
-
+const createUpdateQuiz = async (req: VercelRequest, res: VercelResponse) => {
   const { quizName, description, passMarkPercentage } = req.body;
 
   if (!quizName || !description || !passMarkPercentage) {
@@ -80,4 +71,43 @@ export default async function (req: VercelRequest, res: VercelResponse) {
 
   console.log('Quiz saved: ', uuid);
   res.status(200).send({ error: '' });
+};
+
+const getQuizzesForUser = async (req: VercelRequest, res: VercelResponse) => {
+  const userInfo: UserInfo = await getUserEmailFromAuthToken(req);
+  if (userInfo.error) {
+    return res.status(400).send(userInfo.error);
+  }
+
+  // At this point, we definitely know the user's email
+  const email = userInfo.email ?? '';
+
+  try {
+    const queryText = 'SELECT * FROM public.quiz WHERE created_by_user = $1';
+    const data = await postgresClient.query(queryText, [email]);
+    return res.status(200).send(data?.rows);
+  } catch (err) {
+    const error = err as IPostgresError;
+    console.error('Error querying quiz table in DB: ', error.stack);
+    return res.status(500).send({
+      message: 'Error querying quiz table in DB',
+    });
+  }
+};
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method === 'POST') {
+    return createUpdateQuiz(req, res);
+  }
+
+  if (req.method === 'GET') {
+    return getQuizzesForUser(req, res);
+  }
+
+  res.setHeader('Allow', 'POST');
+  return res.status(405).end('Method Not Allowed');
 }
