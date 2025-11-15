ee8d95e43b888792d867f7c5ff562dfce78541a3
Romesh <romesh@medoment.com>
2025-11-15 08:42:05 +0530

    controller

--- src/pages/api/auth/my-quizzes/index.ts
+++ src/pages/api/auth/my-quizzes/index.ts
@@ -1,7 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { IPostgresError } from '../../../../common/interfaces/Other/IPostgresError';
 import { runCorsMiddleware } from '../../../../common/middleware/cors';
-import postgresClient from '../../../../common/postgres';
 import { getUserEmailFromAuthToken } from '../../../../common/utils/auth';
 import { Quiz } from '../../../../common/types/Quiz';
 import controllerPostgres from '../../../../common/infrastructure/postgres/controllers/postgres-quiz-controller';
@@ -14,21 +12,8 @@ const getQuizzesForUser = async (req: VercelRequest, res: VercelResponse) => {
     return res.status(400).send(userInfo.error);
   }
 
-  // At this point, we definitely know the user's email
-  const email = userInfo.email ?? '';
-
-  try {
-    const queryText =
-      'SELECT * FROM public.quiz WHERE created_by_user = $1 ORDER BY last_updated DESC';
-    const data = await postgresClient.query(queryText, [email]);
-    return res.status(200).send(data?.rows);
-  } catch (err) {
-    const error = err as IPostgresError;
-    console.error('Error querying quiz table in DB: ', error.stack);
-    return res.status(500).send({
-      message: 'Error querying quiz table in DB',
-    });
-  }
+  let response = await controller.getQuizzesForUser(userInfo.email as string);
+  return res.status(200).send(response);
 };
 
 const createQuiz = async (req: VercelRequest, res: VercelResponse) => {
