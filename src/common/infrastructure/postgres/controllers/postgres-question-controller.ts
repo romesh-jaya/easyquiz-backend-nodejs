fd357eacb44a4a9af45f14146d29388660272566
Romesh <romesh@medoment.com>
2025-11-23 15:37:33 +0530

    save quiz order

--- src/common/infrastructure/postgres/controllers/postgres-question-controller.ts
+++ src/common/infrastructure/postgres/controllers/postgres-question-controller.ts
@@ -4,11 +4,13 @@ import GetQuizQuestion from '../../../use-cases/quiz-question/get';
 import UpdateQuizQuestion from '../../../use-cases/quiz-question/update';
 import DeleteQuizQuestion from '../../../use-cases/quiz-question/delete';
 import QuizQuestionPostgresDAO from '../quizQuestion';
+import QuizPostgresDAO from '../quiz';
 
 let quizQuestionDAO = new QuizQuestionPostgresDAO();
+let quizDAO = new QuizPostgresDAO();
 
 let controller = new QuizQuestionController(
-  new CreateQuizQuestion(quizQuestionDAO),
+  new CreateQuizQuestion(quizQuestionDAO, quizDAO),
   new GetQuizQuestion(quizQuestionDAO),
   new UpdateQuizQuestion(quizQuestionDAO),
   new DeleteQuizQuestion(quizQuestionDAO)
--- src/common/use-cases/quiz-question/create.ts
+++ src/common/use-cases/quiz-question/create.ts
@@ -2,9 +2,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { IQuizQuestionDAO } from '../../interfaces/DAO/IQuizQuestionDAO';
 import { IResponse } from '../../interfaces/Other/IResponse';
 import { QuizQuestion } from '../../types/QuizQuestion';
+import { IQuizDAO } from '../../interfaces/DAO/IQuizDAO';
 
 export default class CreateQuizQuestion {
-  constructor(protected quizQuestionDAO: IQuizQuestionDAO) {}
+  constructor(
+    protected quizQuestionDAO: IQuizQuestionDAO,
+    protected quizDAO: IQuizDAO
+  ) {}
 
   async call(data: Partial<QuizQuestion>, userId: string): Promise<IResponse> {
     if (!data.quizId || !data.questionContent || !data.answers) {
@@ -16,6 +20,19 @@ export default class CreateQuizQuestion {
     const uuid = uuidv4();
     data.id = uuid;
 
-    return this.quizQuestionDAO.create(data, userId);
+    let questionResponse = await this.quizQuestionDAO.create(data, userId);
+
+    if (!questionResponse.error) {
+      let quiz = await this.quizDAO.get(data.quizId as string, userId);
+      let existingQuestionOrder = (quiz as any).question_order || [];
+      existingQuestionOrder.push(uuid);
+      await this.quizDAO.updateQuestionOrder(
+        data.quizId as string,
+        existingQuestionOrder,
+        userId
+      );
+    }
+
+    return questionResponse;
   }
 }
